7e0ffe13407e4e81e55182a9280e1f0ebae11b1f
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2024-04-08 03:39:17 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -63,20 +63,3 @@ export class App extends Component {
     );
   }
 }
-
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101',
-//       }}
-//     >
-//       React homework template
-//     </div>
-//   );
-// };
